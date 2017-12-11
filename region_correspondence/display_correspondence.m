bbc51d74d3a03983c766a6bea3c05f32b31e9f93
Sergios Stamatis <mindosoft@gmail.com>
2017-12-11 10:32:39 +0200

    Enhance display correspondence (to display frame as title and allow use of other videoIds and frame start)

--- region_correspondence/display_correspondence.m
+++ region_correspondence/display_correspondence.m
@@ -1,7 +1,7 @@
 clear
 clc
 
-videoID = 1449;
+videoID = 1;
 r = 425;
 c = 560;
 
@@ -10,10 +10,21 @@ csp(1,:) = 0;
 
 files = dir(sprintf('./correspondences/%04d',videoID));
 
+frameStep = 3;
+startFrame = 1;
+targetFrame = 151;
+
+figure;
+
 for i = 3:length(files)
     name = files(i).name;
     corr = load(sprintf('./correspondences/%04d/%s',videoID,name));
     corr = reshape(corr,[r,c]);
     imshow(ind2rgb(corr, im2double(uint8(csp))));
+    if ((((i - 3) * frameStep) + startFrame) == targetFrame)
+      title(sprintf('Target Frame %05d', ((i - 3) * frameStep) + startFrame));  
+    else
+      title(sprintf('Frame %05d', ((i - 3) * frameStep) + startFrame));
+    end
     drawnow;
 end
